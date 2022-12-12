74e47c89cb423e54f6a0e035b34e7a4e569d51dc
Mustafa Oral <drumex@gmail.com>
2022-12-12 19:18:00 +0000

    Day 10 of 2022 alternative

--- src/AdventOfCode/ExtensionMethods/TwoDimensionalArrayExtensions.cs
+++ src/AdventOfCode/ExtensionMethods/TwoDimensionalArrayExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.ExtensionMethods;
+﻿using System.Text;
+
+namespace AdventOfCode.ExtensionMethods;
 
 public static class TwoDimensionalArrayExtensions
 {
@@ -81,5 +83,52 @@ public static class TwoDimensionalArrayExtensions
                 yield return new(i, j, array[i, j]);
             }
         }
+    }
+
+    public static string AsMatrixText<T>(this T[,] array, Predicate<T> predicate, Func<T, char> trueTransform, char falseValue)
+    {
+        return AsMatrixText(array, predicate, trueTransform, _ => falseValue);
+    }
+
+    public static string AsMatrixText<T>(this T[,] array, Predicate<T> predicate, char trueValue, Func<T, char> falseTransform)
+    {
+        return AsMatrixText(array, predicate, _ => trueValue, falseTransform);
+    }
+
+    public static string AsMatrixText<T>(this T[,] array, Predicate<T> predicate, char trueValue, char falseValue)
+    {
+        return AsMatrixText(array, predicate, _ => trueValue, _ => falseValue);
+    }
+
+    public static string AsMatrixText<T>(this T[,] array, Predicate<T> predicate, Func<T, char> trueTransform, Func<T, char> falseTransform)
+    {
+        var height = array.GetLength(0);
+        var width = array.GetLength(1);
+
+        var sb = new StringBuilder(height * width);
+
+        for (int i = 0; i < height; i++)
+        {
+            if (i > 0)
+            {
+                sb.AppendLine();
+            }
+
+            for (int j = 0; j < width; j++)
+            {
+                var item = array[i, j];
+
+                if (predicate(item))
+                {
+                    sb.Append(trueTransform(item));
+                }
+                else
+                {
+                    sb.Append(falseTransform(item));
+                }
+            }
+        }
+
+        return sb.ToString();
     }
 }
---
+++ src/AdventOfCode2022/Day10Alternative.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode2022;
+
+public class Day10Alternative : IStringInputChallenge<int, string>
+{
+    public int Part1(string input)
+    {
+        var instructions = GetInstructions(input);
+
+        return Enumerable.Range(0, 6)
+            .Select(x => 20 + x * 40)
+            .Sum(cycle => (instructions.Where(x => x.StartCycle + x.Duration < cycle).Sum(x => x.Change) + 1) * cycle);
+    }
+
+    public string Part2(string input)
+    {
+        var instructions = GetInstructions(input);
+
+        return Enumerable.Range(0, 240)
+            .Aggregate(seed: new bool[6, 40], (accumulate, cycle) =>
+            {
+                var spriteStartIndexAtCycle = instructions.Where(x => x.StartCycle + x.Duration <= cycle).Sum(x => x.Change);
+
+                accumulate[cycle / 40, cycle % 40] = cycle % 40 >= spriteStartIndexAtCycle && cycle % 40 <= spriteStartIndexAtCycle + 2;
+
+                return accumulate;
+            }, x => x.AsMatrixText(predicate: x => x, trueValue: '#', falseValue: '.'));
+    }
+
+    private static Instruction[] GetInstructions(string input)
+    {
+        var instructions = input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select((x, i) =>
+            {
+                if (x == "noop")
+                {
+                    return new Instruction();
+                }
+
+                return new Instruction { Duration = 2, Change = int.Parse(x[4..]) };
+            })
+            .ToArray();
+
+        instructions.Pairwise(x => x.Second.StartCycle = x.First.StartCycle + x.First.Duration);
+
+        return instructions;
+    }
+
+    private class Instruction
+    {
+        public int StartCycle { get; set; }
+        public int Duration { get; set; } = 1;
+        public int Change { get; set; }
+    }
+}
